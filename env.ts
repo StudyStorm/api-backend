3e3b08964feb19e91f2901114513c7bfe12d0746
Maxime Scharwath <maxscharwath@gmail.com>
2022-08-25 09:04:01 +0200

    Fb: Add S3 driver (#1)
    * fb: Add s3 driver
    
    * format eslint

--- env.ts
+++ env.ts
@@ -19,7 +19,7 @@ export default Env.rules({
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
-  DRIVE_DISK: Env.schema.enum(["local"] as const),
+  DRIVE_DISK: Env.schema.enum(["local", "s3"] as const),
   NODE_ENV: Env.schema.enum(["development", "production", "test"] as const),
 
   PG_HOST: Env.schema.string({ format: "host" }),
