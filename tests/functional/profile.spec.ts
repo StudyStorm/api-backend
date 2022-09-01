0278ca9b89ca95700bf449278e06b3e551068bde
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-09-01 13:34:17 +0200

    feat: tests done for profile

--- tests/functional/profile.spec.ts
+++ tests/functional/profile.spec.ts
@@ -8,8 +8,14 @@ test.group("Profile", (group) => {
     return () => Database.rollbackGlobalTransaction();
   });
 
-  test("Should fail if user  is not logged in", async ({ client }) => {
-    const response = await client.get("v1/profile");
+  test("Should fail if user is not logged in", async ({ client }) => {
+    let response = await client.get("v1/profile");
+    response.assertStatus(401);
+
+    response = await client.patch("v1/profile");
+    response.assertStatus(401);
+
+    response = await client.delete("v1/profile");
     response.assertStatus(401);
   });
 
@@ -34,6 +40,80 @@ test.group("Profile", (group) => {
       first_name: "Test",
       last_name: "User",
     });
+    response.assertStatus(200);
+  });
+
+  test("Should update user profile", async ({ client }) => {
+    await User.create({
+      email: "test@studystorm.net",
+      password: "test123",
+      firstName: "Test",
+      lastName: "User",
+    });
+
+    const login = await client.post("v1/login").json({
+      email: "test@studystorm.net",
+      password: "test123",
+    });
+
+    const response = await client
+      .patch("v1/profile")
+      .json({
+        firstName: "Testing",
+        lastName: "Patch",
+      })
+      .session(login.session());
+
+    response.assertStatus(200);
+    response.assertBodyContains({
+      first_name: "Testing",
+      last_name: "Patch",
+    });
+  });
+
+  test("Should not modifiy unspecified keys", async ({ client }) => {
+    await User.create({
+      email: "test@studystorm.net",
+      password: "test123",
+      firstName: "Test",
+      lastName: "User",
+    });
+
+    const login = await client.post("v1/login").json({
+      email: "test@studystorm.net",
+      password: "test123",
+    });
+
+    const response = await client
+      .patch("v1/profile")
+      .json({
+        firstName: "Testing",
+        lastName: "Patch",
+      })
+      .session(login.session());
+
+    response.assertStatus(200);
+    response.assertBodyContains({
+      first_name: "Testing",
+      last_name: "Patch",
+      email: "test@studystorm.net",
+    });
+  });
+
+  test("Should delete user account", async ({ client }) => {
+    await User.create({
+      email: "test@studystorm.net",
+      password: "test123",
+      firstName: "Test",
+      lastName: "User",
+    });
+
+    const login = await client.post("v1/login").json({
+      email: "test@studystorm.net",
+      password: "test123",
+    });
+
+    const response = await client.delete("v1/profile").session(login.session());
     response.assertStatus(200);
   });
 });
