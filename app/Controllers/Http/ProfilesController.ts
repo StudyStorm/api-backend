fb4787417ef1407b0e67ff106813971a28277b2a
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-09-01 12:07:26 +0200

    Created shema for updating profile

--- app/Controllers/Http/ProfilesController.ts
+++ app/Controllers/Http/ProfilesController.ts
@@ -1,7 +1,25 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import ProfilesUpdateSchema from "App/Schemas/ProfilesUpdateSchema";
+import User from "App/Models/User";
 
 export default class ProfilesController {
   public async index({ auth }: HttpContextContract) {
     return auth.user;
+  }
+
+  public async update({ auth, request }: HttpContextContract) {
+    const user = await User.findOrFail(auth.user!.id);
+
+    const payload = await request.validate({
+      schema: ProfilesUpdateSchema,
+    });
+
+    return await user.merge(payload).save();
+  }
+
+  public async destroy({ auth, response }: HttpContextContract) {
+    const user = await User.findOrFail(auth.user!.id);
+    await user.delete();
+    return response.ok({ message: "Account deleted successfully" });
   }
 }
