6388af95b9829fbae10af5f8768e4ccd82a5b73c
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-09-02 16:12:42 +0200

    fix: lint

--- database/factories/DeckFactory.ts
+++ database/factories/DeckFactory.ts
@@ -1,7 +1,6 @@
 import Deck from "App/Models/Deck";
 import Factory from "@ioc:Adonis/Lucid/Factory";
 import { CardFactory } from "./CardFactory";
-import { RatingFactory } from "Database/factories/RatingFactory";
 import { UserFactory } from "Database/factories/UserFactory";
 
 export const DeckFactory = Factory.define(Deck, ({ faker }) => ({
