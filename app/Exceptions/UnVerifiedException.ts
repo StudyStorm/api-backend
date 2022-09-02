f7d2734238768acae03c836e4de360e7dc6ab43a
Maxime Scharwath <maxscharwath@gmail.com>
2022-09-03 01:38:17 +0200

    return 403 forbidden when not verified and generate token

--- app/Exceptions/UnVerifiedException.ts
+++ app/Exceptions/UnVerifiedException.ts
@@ -1,4 +1,7 @@
 import { Exception } from "@adonisjs/core/build/standalone";
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Encryption from "@ioc:Adonis/Core/Encryption";
+import User from "App/Models/User";
 
 /*
 |--------------------------------------------------------------------------
@@ -12,4 +15,18 @@ import { Exception } from "@adonisjs/core/build/standalone";
 | new UnVerifiedException('message', 500, 'E_RUNTIME_EXCEPTION')
 |
 */
-export default class UnVerifiedException extends Exception {}
+export default class UnVerifiedException extends Exception {
+  constructor(public user: User) {
+    super("User is not verified", 403);
+  }
+  public async handle(error: this, { response }: HttpContextContract) {
+    return response.forbidden({
+      message: error.message,
+      resendToken: Encryption.encrypt(
+        error.user.id,
+        "5 minutes",
+        "resendToken"
+      ),
+    });
+  }
+}
